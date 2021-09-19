b52ee2c660b7389566791bf6478b38069f62aafb
Roberto Mendoza <roberto.cmp16@gmail.com>
2021-09-19 21:58:58 +0000

    added in models.py tables Vechicles and Favorites,a added router in app.py to vehicles

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os 
 from flask import Flask,jsonify,request
 from flask_sqlalchemy import SQLAlchemy
-from models import db,User
+from models import db,User,Vehicles
 from flask_migrate import Migrate
 #from flask_script import Manager
 
@@ -41,6 +41,27 @@ def user():
     
 
     return jsonify(user.serialize()),200
+
+@app.route("/vehicles",methods=["POST","GET"])
+def vehicles():
+    if request.method == "GET":
+        vehicles = Vehicles.query.get(1)
+        if vehicles is not None:
+            return jsonify(vehicles.serialize())   
+    else:
+        vehicles = Vehicles()
+        data = request.json.get("name")
+        vehicles.name = request.json.get("name")
+        vehicles.model = request.json.get("model")
+        vehicles.manufacturer= request.json.get("manufacturer")
+        vehicles.passengers=request.json.get("passengers")
+        vehicles.vehicle_class=request.json.get("vehicle_class")
+        
+        db.session.add(vehicles)
+        db.session.commit()
+    
+
+    return jsonify(vehicles.serialize()),200
 
 
 
--- models.py
+++ models.py
@@ -19,12 +19,61 @@ class User(db.Model):
             'firstname':self.firstname,
             'lastname':self.lastname,
             'password': self.password,
-            'email': self.email,
-            'isActive': self.isActive
+            'email': self.email
         }
     def serialize_just_username(self):
         return {
         'id': self.id,
         'firstname': self.firstname,
         'lastname':self.lastname
+        }
+
+class Vehicles(db.Model):
+    __tablename__ = 'vehicles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    model = db.Column(db.String(50))
+    manufacturer = db.Column(db.String(50))
+    passengers = db.Column(db.String(30))
+    vehicle_class = db.Column(db.String(50))
+
+    def __repr__(self):
+        return "<Vehicles %r>" % self.id
+    
+    def serialize(self):
+        return {
+            'id':self.id,
+            'name':self.name,
+            'model':self.model,
+            'manufacturer': self.manufacturer,
+            'passengers': self.passengers,
+            'vehicle_class': self.vehicle_class
+        }
+    def serialize_just_vehicles(self):
+        return {
+        'id': self.id,
+        'name': self.name,
+        'model':self.model
+        }
+
+class Favorites(db.Model):
+    __tablename__ = 'favorites'
+    id = db.Column(db.Integer, primary_key=True)
+    category = db.Column(db.String(30))
+    favorite_name = db.Column(db.String(50))
+
+    def __repr__(self):
+        return "<Favorites %r>" % self.id
+    
+    def serialize(self):
+        return {
+            'id':self.id,
+            'category':self.category,
+            'favorite_name':self.favorite_name
+        }
+    def serialize_just_favorites(self):
+        return {
+        'id': self.id,
+        'category': self.category,
+        'favorite_name':self.favorite_name
         }
